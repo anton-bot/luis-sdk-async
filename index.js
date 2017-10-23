2c550b3d6e02d401b00920e911f262bbcc54afde
Anton Ivanov <anton@imago.ai>
2017-10-23 18:39:23 +0800

    implement my own methods to call LUIS. to make my life easier

--- index.js
+++ index.js
@@ -63,6 +63,64 @@ class LuisAsync {
       }, forceSetParameterName);
     });
   }
+
+  /**
+   * Sends the `text` to LUIS server and stores the response inside this
+   * instance.
+   * @param {string} text - The message from the user which we need to send to
+   *    LUIS to recognize the intent.
+   */
+  async send(text) {
+    // Erase previous response and re-query the server:
+    this.response = null;
+    this.response = await this.predict(text);
+  }
+
+  /**
+   * Returns the top scoring intent from the LUIS response.
+   * @returns {string} The name of the top scoring intent, e.g. "None";
+   */
+  intent() {
+    this._verifyResponseReceived();
+    return this.response.topScoringIntent.intent;
+  }
+
+  /**
+   * Returns the value of the first entity of the given type. If there are
+   * multiple entities of that type, only the first one is returned.
+   * @param {string} entityType - The entity type, e.g. "CityName";
+   * @returns {string} The value of the first entity of the specified type,
+   *  or null if the entity was not found.
+   */
+  entity(entityType) {
+    this._verifyResponseReceived();
+
+    // Loop through all entities and find one of the given type:
+    for (let entity of this.response.entities) {
+      if (entity.type === entityType) {
+        // Check if the entity has a canonical form. If so, return the
+        // canonical form. Otherwise just return whatever was recognized.
+        if (entity.resolution && entity.resolution.values) {
+          return entity.resolution.values[0];
+        } else {
+          return entity.entity;
+        }
+      }
+    }
+
+    return null;
+  }
+
+  /**
+   * Checks that we have previous called LUIS endpoint, and received the
+   * response successfully.
+   */
+  _verifyResponseReceived() {
+    if (!this.response || !this.response.topScoringIntent) {
+      throw new Error('We don\'t have any LUIS response yet. Make sure to ' +
+        'await luis.send(text) first.');
+    }
+  }
 }
 
 module.expors = LuisAsync;
